1a996d6235aaa6554bb3bc6599ffb7933e6bcd07
Marcos <marcosviniciohd@hotmail.com>
2023-09-15 11:34:14 -0300

    Curso de Lógica com JavaScript atualizado

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,29 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 30');
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 5;
+// let chute;
+// console.log(numeroSecreto);
+
+// while (chute != numeroSecreto) {
+//     chute = parseInt(prompt('Digite um número entre 0 e 10'));
+//     if( chute === numeroSecreto) {
+//         alert(`Você descobriu o número secreto  ${numeroSecreto}`);
+//     } else if (chute > numeroSecreto) {
+//         alert('O número secreto é menor');
+//     } else if (chute < numeroSecreto) {
+//         alert('O número secreto é maior');
+//     }
+// }
+
+
+console.clear();
+alert(`Boas vindas ao jogo do número secreto`);
+let numeroSecreto = parseInt(Math.random() * 11);
 console.log(numeroSecreto);
+let chute;
 
-if( chute == numeroSecreto) {
-    console.log('Você descobriu o número secreto' + numeroSecreto);
-} else {
-    console.log('Você errou!');
+while (chute != numeroSecreto) {
+    chute = parseInt(prompt(`Digite o número secreto`));
+    if (chute == numeroSecreto) alert(`Você acertou o número secreto, parabéns! O número secreto é ${numeroSecreto}`);
+    else if (chute > numeroSecreto) alert(`O número secreto é menor`);
+    else if (chute < numeroSecreto) alert(`O número secreto é maior`);
 }
-
-
