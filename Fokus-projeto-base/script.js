ee1d80e67671567f22c214d5ff01db5c7978b783
Marcos <marcosviniciohd@hotmail.com>
2023-10-20 11:34:45 -0300

    Update projeto Fokus - Formação JavaScript Web

---
+++ Fokus-projeto-base/script.js
@@ -0,0 +1,21 @@
+const modo = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const focoCurtoBt = document.querySelector('.app__card-button--curto');
+const focoLongoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+
+focoBt.addEventListener('click', () => {
+    modo.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
+});
+
+focoCurtoBt.addEventListener('click', () => {
+    modo.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
+});
+
+focoLongoBt.addEventListener('click', () => {
+    modo.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
+});
+
