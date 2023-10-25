d65fc3bd7b8b873b8c81c81e0f1dd8f0404107db
Marcos <marcosviniciohd@hotmail.com>
2023-10-25 10:02:10 -0300

    classList, audio, play, pause

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,20 +4,41 @@ const focoCurtoBt = document.querySelector('.app__card-button--curto');
 const focoLongoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 });
 
 focoCurtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    focoCurtoBt.classList.add('active');
+
 });
 
 focoLongoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    focoLongoBt.classList.add('active');
+
 });
 
 function alterarContexto(contexto){
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active');
+    });
     modo.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
@@ -29,7 +50,7 @@ function alterarContexto(contexto){
             break;
         case 'descanso-curto':
             titulo.innerHTML = `
-                Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curtaF</strong>
+                Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta.</strong>
             `
             break;
         case 'descanso-longo':
